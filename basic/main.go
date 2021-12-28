ccfe5c3b9289b2ce847d403d61a25fe538dc237e
zhe.peng <zhe.peng@shopee.com>
2021-12-28 20:11:19 +0800

    test commit

--- basic/main.go
+++ basic/main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 	"unicode/utf8"
 )
 
 func main() {
 	fmt.Println("1625220821")
+
+	payload := strings.NewReader(fmt.Sprintf(
+		`{"slo_id": "%s"}`, strconv.FormatInt(int64(1204653840216), 10)))
+
+	itoa := strconv.Itoa(int(1204653840216))
+
+	fmt.Println(itoa)
+	fmt.Println("payload")
+	fmt.Println(payload)
+	fmt.Println(payload.Size())
+	fmt.Println("-----------")
 	timeNow := time.Unix(1625220821, 0) //2017-08-30 16:19:19 +0800 CST
 
 	timeString := timeNow.Format("2006-01-02 15:04:05") //2015-06-15 08:52:32
 	fmt.Println(timeString)
-	fmt.Println(1 << 6)
-	fmt.Println(1 << 2)
-	fmt.Println(1 << 7)
+	fmt.Println(1 << 1)
+	fmt.Println(1 << 4)
 	fmt.Println(1 << 5)
+	fmt.Println(1 << 7)
+	fmt.Println(1 << 8)
 
 	ss := "Av.limonita , 1181, "
 	fmt.Println(ss)
