753ef9aa74520b7077ab369993881a73a14f835e
Zhe Peng <zhe.peng@shopee.com>
2022-07-05 10:14:55 +0800

    test commit

--- map/main.go
+++ map/main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("test")
-	var m map[string]int = map[string]int{"234": 1}
+	var m = map[string]int{"234": 1}
 	i, ok := m["234"]
 	fmt.Println(i, ok)
+
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("求和结果:", sum)
 }
