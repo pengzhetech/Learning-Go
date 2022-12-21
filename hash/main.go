b9862bc70626538f67a63688687becc5b3a371e0
Zhe Peng <zhe.peng@shopee.com>
2022-12-21 11:31:04 +0800

    test commit

--- hash/main.go
+++ hash/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"strings"
 )
 
 func main() {
 
 	hash := CRC32("35634654654654")
 	fmt.Println(hash & (128 - 1))
+
+	i := xiaoshu("123.0")
+	fmt.Println(i)
 }
 func CRC32(str string) uint32 {
 	return crc32.ChecksumIEEE([]byte(str))
+}
+
+func xiaoshu(a string) int {
+	numstr := fmt.Sprint(a)
+	tmp := strings.Split(numstr, ".")
+	if len(tmp) <= 1 {
+		return 0
+	}
+	return len(tmp[1])
 }
